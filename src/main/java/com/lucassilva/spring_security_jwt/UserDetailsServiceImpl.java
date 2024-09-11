2bf675f8c2bb4f48d1bfff7d110d7073ff144b27
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-09-11 17:13:00 -0300

    feat: add data insertion and add service annotation to serviceImpl

--- src/main/java/com/lucassilva/spring_security_jwt/UserDetailsServiceImpl.java
+++ src/main/java/com/lucassilva/spring_security_jwt/UserDetailsServiceImpl.java
@@ -3,7 +3,9 @@ package com.lucassilva.spring_security_jwt;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
